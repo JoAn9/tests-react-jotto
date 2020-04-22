e8b9e5043ebaaf6a296b5b58982c6c94471f50f4
JoAn <joannacalinska@gmail.com>
2020-04-22 22:30:05 +0200

    getSecretWord on component did mount

--- src/App.js
+++ src/App.js
@@ -2,8 +2,31 @@ import React from 'react';
 import './App.css';
 import GuessedWords from './GuessedWords';
 import Congrats from './Congrats';
+import hookActions from './actions/hookActions';
+
+const initialState = {
+  secretWord: null,
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'setSecretWord':
+      return { ...state, secretWord: action.payload };
+    default:
+      return state;
+  }
+}
 
 function App() {
+  const [state, dispatch] = React.useReducer(reducer, initialState);
+
+  const setSecretWord = secretWord =>
+    dispatch({ type: 'setSecretWord', payload: secretWord });
+
+  React.useEffect(() => {
+    hookActions.getSecretWord(setSecretWord);
+  }, []);
+
   return <div className="container" data-test="component-app"></div>;
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,26 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import App from './App';
+import hookActions from './actions/hookActions';
+
+const mockGetSecretWord = jest.fn();
+
+const setup = () => {
+  mockGetSecretWord.mockClear();
+  hookActions.getSecretWord = mockGetSecretWord;
+
+  return mount(<App />);
+};
 
 test('App renders without error', () => {
-  const wrapper = shallow(<App />);
+  const wrapper = setup();
   const component = wrapper.find('[data-test="component-app"]');
   expect(component.length).toBe(1);
+});
+
+describe('getSecretWord calls', () => {
+  test('getSecretWord gets called on App mount', () => {
+    setup();
+    expect(mockGetSecretWord).toHaveBeenCalled();
+  });
 });
